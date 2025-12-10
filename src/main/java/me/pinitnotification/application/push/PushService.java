3e0c4d39ed75496a1e46e849ee1c21e2ce02fb92
GoGradually <chkun3109@gmail.com>
2025-12-10 23:35:32 +0900

    feat: 구독 및 구독 해지 메서드 추가

--- src/main/java/me/pinitnotification/application/push/PushService.java
+++ src/main/java/me/pinitnotification/application/push/PushService.java
@@ -2,6 +2,7 @@ package me.pinitnotification.application.push;
 
 public interface PushService {
     String getVapidPublicKey();
-    void subscribe(String endpoint, String p256dh, String auth);
+    void subscribe(Long memberId, String token);
+    void unsubscribe(Long memberId, String token);
     void sendPushMessage(String token, String title, String body);
 }
