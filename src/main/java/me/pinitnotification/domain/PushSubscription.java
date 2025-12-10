5a8061bda92394c856382ce38755127f6475d23a
GoGradually <chkun3109@gmail.com>
2025-12-10 23:35:39 +0900

    feat: 푸시 구독 엔티티 추가

---
+++ src/main/java/me/pinitnotification/domain/PushSubscription.java
@@ -0,0 +1,28 @@
+package me.pinitnotification.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(
+    uniqueConstraints = {
+        @UniqueConstraint(
+            name = "uk_token_memberId",
+            columnNames = {"member_id", "token"}
+        )
+    }
+)
+public class PushSubscription {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name="token", nullable = false)
+    private String token;
+    @Column(name="member_id", nullable = false)
+    private Long memberId;
+    protected PushSubscription() {}
+    public PushSubscription(Long memberId, String token) {
+        this.memberId = memberId;
+        this.token = token;
+    }
+}
