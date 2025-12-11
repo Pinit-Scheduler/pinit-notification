0bbc701fa82a56210afb49626b9a783eb6929c1e
GoGradually <chkun3109@gmail.com>
2025-12-11 14:46:42 +0900

    feat: CORS 설정을 위한 CorsProperties 클래스 추가

---
+++ src/main/java/me/pinitnotification/infra/authenticate/config/CorsProperties.java
@@ -0,0 +1,21 @@
+package me.pinitnotification.infra.authenticate.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "cors")
+public class CorsProperties {
+
+    private List<String> allowedOrigins;
+    private List<String> allowedMethods;
+    private List<String> allowedHeaders;
+    private Boolean allowCredentials;
+    private Long maxAge;
+}
