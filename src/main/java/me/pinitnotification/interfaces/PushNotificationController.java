a2c5dbe8f602bec7d7db2ff247f029eada244881
GoGradually <chkun3109@gmail.com>
2025-12-11 14:54:33 +0900

    feat: 푸시 알림 구독 및 구독 해제를 위한 메서드 추가

--- src/main/java/me/pinitnotification/interfaces/PushNotificationController.java
+++ src/main/java/me/pinitnotification/interfaces/PushNotificationController.java
@@ -1,5 +1,7 @@
 package me.pinitnotification.interfaces;
 
+import me.pinitnotification.application.push.PushService;
+import me.pinitnotification.domain.member.MemberId;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,14 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/push")
 public class PushNotificationController {
+    private final PushService pushService;
+
+    public PushNotificationController(PushService pushService) {
+        this.pushService = pushService;
+    }
+
     @GetMapping("/vapid")
     public String getVapidPublicKey() {
         return "YOUR_VAPID_PUBLIC_KEY";
     }
 
     @PostMapping("/subscribe")
-    public void subscribe() {
-        // Handle subscription logic here
+    public void subscribe(@MemberId Long memberId, String token) {
+        pushService.subscribe(memberId, token);
     }
 
+    @PostMapping("/unsubscribe")
+    public void unsubscribe(@MemberId Long memberId, String token) {
+        pushService.unsubscribe(memberId, token);
+    }
 }
