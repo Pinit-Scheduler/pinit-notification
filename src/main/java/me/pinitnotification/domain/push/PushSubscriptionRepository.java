0483f23c951e7ec7c33910902234d0659ac98494
GoGradually <chkun3109@gmail.com>
2025-12-14 19:51:50 +0900

    feat: PushSubscriptionRepository에 memberId로 구독 목록 조회 및 토큰 삭제 메서드 추가

--- src/main/java/me/pinitnotification/domain/push/PushSubscriptionRepository.java
+++ src/main/java/me/pinitnotification/domain/push/PushSubscriptionRepository.java
@@ -2,5 +2,10 @@ package me.pinitnotification.domain.push;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PushSubscriptionRepository extends JpaRepository<PushSubscription, Long> {
+    List<PushSubscription> findAllByMemberId(Long memberId);
+
+    void deleteByToken(String token);
 }
