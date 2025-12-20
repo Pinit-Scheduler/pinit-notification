eee9ef5ec304ca906ec8908c305049ef2458e635
GoGradually <chkun3109@gmail.com>
2025-12-20 20:15:05 +0900

    test: PinitNotificationApplicationTests에 테스트용 JwtTokenProvider 추가

--- src/test/java/me/pinitnotification/PinitNotificationApplicationTests.java
+++ src/test/java/me/pinitnotification/PinitNotificationApplicationTests.java
@@ -1,8 +1,15 @@
 package me.pinitnotification;
 
+import me.pinitnotification.infrastructure.authenticate.JwtTokenProvider;
+import me.pinitnotification.utils.TestKeys;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.security.PublicKey;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -12,4 +19,12 @@ class PinitNotificationApplicationTests {
     void contextLoads() {
     }
 
+    @TestConfiguration
+    static class TestBeans {
+        @Bean
+        @Primary
+        JwtTokenProvider testJwtTokenProvider() {
+            return new JwtTokenProvider(TestKeys.publicKey());
+        }
+    }
 }
