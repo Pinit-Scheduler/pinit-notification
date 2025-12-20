0ff557f526d445962124a7caf739586bab38f0f0
GoGradually <chkun3109@gmail.com>
2025-12-20 20:25:17 +0900

    test: PinitNotificationApplicationTests에 FirebaseMessaging 모의 객체 추가

--- src/test/java/me/pinitnotification/PinitNotificationApplicationTests.java
+++ src/test/java/me/pinitnotification/PinitNotificationApplicationTests.java
@@ -1,5 +1,9 @@
 package me.pinitnotification;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
 import me.pinitnotification.infrastructure.authenticate.JwtTokenProvider;
 import me.pinitnotification.utils.TestKeys;
 import org.junit.jupiter.api.Test;
@@ -8,7 +12,12 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.PublicKey;
 
 @ActiveProfiles("test")
@@ -21,6 +30,10 @@ class PinitNotificationApplicationTests {
 
     @TestConfiguration
     static class TestBeans {
+
+        @MockitoBean
+        FirebaseMessaging firebaseMessaging;
+
         @Bean
         @Primary
         JwtTokenProvider testJwtTokenProvider() {
