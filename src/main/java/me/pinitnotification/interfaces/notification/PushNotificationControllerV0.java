7867b1ee84fa26a0f33e2787dc8545f03789e621
GoGradually <chkun3109@gmail.com>
2025-12-31 17:12:43 +0900

    feat: 푸시 알림 API 버전 0 구현

---
+++ src/main/java/me/pinitnotification/interfaces/notification/PushNotificationControllerV0.java
@@ -0,0 +1,69 @@
+package me.pinitnotification.interfaces.notification;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import me.pinitnotification.application.push.PushService;
+import me.pinitnotification.domain.member.MemberId;
+import me.pinitnotification.interfaces.notification.dto.PushTokenRequest;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v0/push")
+@Tag(name = "푸시 알림", description = "푸시 구독 및 VAPID 키 관련 API")
+public class PushNotificationControllerV0 {
+    private final PushService pushService;
+
+    public PushNotificationControllerV0(PushService pushService) {
+        this.pushService = pushService;
+    }
+
+    @GetMapping("/vapid")
+    @Operation(
+            summary = "VAPID 공개키 조회",
+            description = "웹 푸시 구독에 사용되는 VAPID 공개키를 반환합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "VAPID 공개키",
+                    content = @Content(mediaType = "text/plain", schema = @Schema(implementation = String.class)))
+    })
+    public String getVapidPublicKey() {
+        return pushService.getVapidPublicKey();
+    }
+
+    @PostMapping("/subscribe")
+    @Operation(
+            summary = "푸시 토큰 구독 등록",
+            description = "인증된 회원의 푸시 토큰을 등록합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "구독 등록 완료"),
+            @ApiResponse(responseCode = "400", description = "토큰이 유효하지 않음", content = @Content)
+    })
+    public void subscribe(
+            @Parameter(hidden = true) @MemberId Long memberId,
+            @RequestBody PushTokenRequest request) {
+        pushService.subscribe(memberId, request.token());
+    }
+
+    @PostMapping("/unsubscribe")
+    @Operation(
+            summary = "푸시 토큰 구독 해지",
+            description = "인증된 회원의 등록된 푸시 토큰을 삭제합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "구독 해지 완료"),
+            @ApiResponse(responseCode = "400", description = "토큰이 유효하지 않음", content = @Content)
+    })
+    public void unsubscribe(
+            @Parameter(hidden = true) @MemberId Long memberId,
+            @RequestBody PushTokenRequest request) {
+        pushService.unsubscribe(memberId, request.token());
+    }
+
+
+}
