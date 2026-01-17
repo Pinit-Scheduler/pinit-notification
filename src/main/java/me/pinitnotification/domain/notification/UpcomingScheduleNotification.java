1102c1216970778548a5dad67cb6dad5a09a852d
GoGradually <chkun3109@gmail.com>
2026-01-17 22:49:31 +0900

    feat: UpcomingScheduleNotification에서 UUID를 사용하도록 변경

--- src/main/java/me/pinitnotification/domain/notification/UpcomingScheduleNotification.java
+++ src/main/java/me/pinitnotification/domain/notification/UpcomingScheduleNotification.java
@@ -1,38 +1,42 @@
 package me.pinitnotification.domain.notification;
 
-
-import jakarta.persistence.*;
 import lombok.Getter;
 
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeParseException;
 import java.util.Map;
+import java.util.UUID;
 
 @Getter
-@Entity
-@Table(
-        name = "upcoming_schedule_notification",
-        uniqueConstraints = {
-                @UniqueConstraint(name = "uk_schedule_owner", columnNames = {"schedule_id", "owner_id"}),
-                @UniqueConstraint(name = "uk_idempotent_key", columnNames = {"idempotent_key"})
-        }
-)
 public class UpcomingScheduleNotification implements Notification {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(name = "owner_id", nullable = false)
+    private Long legacyId;
+    private UUID id;
     private Long ownerId;
-    @Column(name = "schedule_id", nullable = false)
     private Long scheduleId;
-    @Column(name = "schedule_title", nullable = false)
     private String scheduleTitle;
-    @Column(name = "schedule_start_time", nullable = false)
     private String scheduleStartTime;
-    @Column(name = "idempotent_key", nullable = false)
     private String idempotentKey;
 
     protected UpcomingScheduleNotification() {}
+
+    public UpcomingScheduleNotification(Long legacyId, UUID id, Long ownerId, Long scheduleId, String scheduleTitle, String scheduleStartTime, String idempotentKey) {
+        this.legacyId = legacyId;
+        this.id = id;
+        this.ownerId = ownerId;
+        this.scheduleId = scheduleId;
+        this.scheduleTitle = scheduleTitle;
+        this.scheduleStartTime = scheduleStartTime;
+        this.idempotentKey = idempotentKey;
+    }
+
+    public UpcomingScheduleNotification(UUID id, Long ownerId, Long scheduleId, String scheduleTitle, String scheduleStartTime, String idempotentKey) {
+        this.id = id;
+        this.ownerId = ownerId;
+        this.scheduleId = scheduleId;
+        this.scheduleTitle = scheduleTitle;
+        this.scheduleStartTime = scheduleStartTime;
+        this.idempotentKey = idempotentKey;
+    }
 
     public UpcomingScheduleNotification(Long ownerId, Long scheduleId, String scheduleTitle, String scheduleStartTime, String idempotentKey) {
         this.ownerId = ownerId;
