eb8b0b093178778b79e29199949cc570088c2c44
GoGradually <chkun3109@gmail.com>
2026-01-18 05:41:40 +0900

    feat: PushSubscriptionEntity의 테이블 이름을 'push_subscription'으로 변경

--- src/main/java/me/pinitnotification/infrastructure/persistence/push/PushSubscriptionEntity.java
+++ src/main/java/me/pinitnotification/infrastructure/persistence/push/PushSubscriptionEntity.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 
 @Entity
 @Table(
+        name = "push_subscription",
         uniqueConstraints = {
                 @UniqueConstraint(
                         name = "uk_deviceId_memberId",
