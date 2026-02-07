804cd282a3592d47ea4c32a6fd63626d39dac589
GoGradually <chkun3109@gmail.com>
2026-02-08 03:38:03 +0900

    feat: UpcomingScheduleNotification의 scheduleStartTime 타입을 Instant로 변경

--- src/main/java/me/pinitnotification/domain/notification/UpcomingScheduleNotificationRepository.java
+++ src/main/java/me/pinitnotification/domain/notification/UpcomingScheduleNotificationRepository.java
@@ -1,5 +1,6 @@
 package me.pinitnotification.domain.notification;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,7 +10,7 @@ public interface UpcomingScheduleNotificationRepository {
     boolean existsByScheduleIdAndOwnerId(Long scheduleId, Long ownerId);
     UpcomingScheduleNotification save(UpcomingScheduleNotification notification);
 
-    void updateScheduleStartTimeAndIdempotentKey(Long scheduleId, Long ownerId, String scheduleStartTime, String idempotentKey);
+    void updateScheduleStartTimeAndIdempotentKey(Long scheduleId, Long ownerId, Instant scheduleStartTime, String idempotentKey);
     void deleteByScheduleIdAndOwnerId(Long scheduleId, Long ownerId);
     void deleteAllInBatch(List<UpcomingScheduleNotification> notifications);
 }
